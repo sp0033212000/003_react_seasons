5ce0becf2c9645590dac017bd3255c4af2fdf8a3
Liao Ji Wei <liaojiwei@Liaode-MacBook-Pro.local>
2019-12-01 17:50:47 +0800

    Updates

---
+++ precache-manifest.16f2ad28f1393c70793ffad58b184377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a63b99927c3efe49d06f67bf85df2be",
+    "url": "/003_react_seasons/index.html"
+  },
+  {
+    "revision": "3700757466483e496f38",
+    "url": "/003_react_seasons/static/css/main.2bef5a67.chunk.css"
+  },
+  {
+    "revision": "4784dcefe45683c89c7a",
+    "url": "/003_react_seasons/static/js/2.8d6027dc.chunk.js"
+  },
+  {
+    "revision": "3700757466483e496f38",
+    "url": "/003_react_seasons/static/js/main.75687f7b.chunk.js"
+  },
+  {
+    "revision": "67ad163a9f82dcf4028b",
+    "url": "/003_react_seasons/static/js/runtime-main.897df91a.js"
+  }
+]);
